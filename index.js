111e7d55d289bfbc1ef7634663b75974cc8dcd45
DCartist <pbannerman13@gmail.com>
2019-06-06 14:41:53 -0400

    fixing index

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const parser = require('body-parser')
-
 const app = express()
-
+app.use(parser.urlencoded({ extended: true }))
+app.use(parser.json())
 app.get("/", (req, res) => {
     res.send("Welcome")
 })
-
 app.listen(4040, () => console.log("IT WORKS on 4040!!!!!!!!!!!!!!!"))
