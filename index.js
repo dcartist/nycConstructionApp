61ddf7fdaf0f9eab9f8ef8b9e6c42a342f842d9c
DCartist <pbannerman13@gmail.com>
2019-06-06 14:40:46 -0400

    setting up index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express')
 const parser = require('body-parser')
 
 const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Welcome")
+})
+
+app.listen(4040, () => console.log("IT WORKS on 4040!!!!!!!!!!!!!!!"))
