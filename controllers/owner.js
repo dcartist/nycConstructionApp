e4a0e4ff65791d3b50cfef97932f2b5bb79efd42
DCartist <pbannerman13@gmail.com>
2019-06-07 09:59:57 -0400

    finished some of the routes

---
+++ controllers/owner.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const Owner = require('../models/Owner')
+
+router.get("/", (req, res) => {
+    Owner.find().then(owner => {
+        res.json(owner)
+    })
+})
+
+router.get("/name/:ownLastName", (req, res) => {
+    let theName = req.params.ownLastName
+    Owner.find({ ownLastName: theName }).then(showName => res.json(showName))
+})
+router.get("/type/:info", (req, res) => {
+    let theName = req.params.info
+    Owner.find({ ownType: theName }).then(showName => res.json(showName))
+})
+
+
+router.post("/new", (req, res) => {
+    Owner.create(req.body).then(owner => res.json(owner))
+})
+
+router.put("/update/:id", (req, res) => {
+    Owner.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        .then(update => res.json(update))
+})
+
+router.delete("/delete/:id", (req, res) => {
+    Owner.findOneAndDelete({ _id: req.params.id }).then(deleted => res.json(deleted))
+})
+
+
+module.exports = router
---
+++ controllers/property.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const Property = require('../models/Property')
+
+router.get("/", (req, res) => {
+    Property.find().then(property => {
+        res.json(property)
+    })
+})
+
+router.get("/borough/:borough", (req, res) => {
+    let theName = req.params.borough
+    Property.find({ borough: theName }).then(showName => res.json(showName))
+})
+router.get("/city/:info", (req, res) => {
+    let theName = req.params.info
+    Property.find({ city: theName }).then(showName => res.json(showName))
+})
+
+
+router.post("/new", (req, res) => {
+    Property.create(req.body).then(property => res.json(property))
+})
+
+router.put("/update/:id", (req, res) => {
+    Property.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        .then(update => res.json(update))
+})
+
+router.delete("/delete/:id", (req, res) => {
+    Property.findOneAndDelete({ _id: req.params.id }).then(deleted => res.json(deleted))
+})
+
+
+module.exports = router
