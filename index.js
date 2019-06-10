cda0e90bd1248c2d7e70efd6e95e5cee83961508
DCartist <pbannerman13@gmail.com>
2019-06-09 21:42:55 -0400

    finish cleaning up

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use("/api/owner", ownController)
 app.use("/api/property", propController)
 app.use("/api/job", jobController)
 
-
-
-app.listen(4040, () => console.log("IT WORKS on 4040!!!!!!!!!!!!!!!"))
+app.set("port", process.env.PORT || 4040);
+app.listen(app.get("port"), () => {
+    console.log(`PORT: ${app.get("port")} works`)
+})
