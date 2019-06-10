79dea6fcda15175f4f31bc5582dd54e65d3f683d
DCartist <pbannerman13@gmail.com>
2019-06-09 22:10:42 -0400

    fixing

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose')
-
 mongoose.Promise = Promise
 let mongoURI = ""
 
 if (process.env.NODE_ENV === "production") {
-
     mongoURI = process.env.DB_URL
 } else {
-
     mongoURI = "mongodb://localhost/nycjobapp"
 }
 mongoose
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const parser = require('body-parser')
 const app = express()
+    // const cors = require('cors')
 const conController = require('./controllers/contractor.js')
 const ownController = require('./controllers/owner.js')
 const propController = require('./controllers/property.js')
 const jobController = require('./controllers/job.js')
 app.use(parser.urlencoded({ extended: true }))
 app.use(parser.json())
-
+    // app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Welcome")
@@ -17,7 +18,14 @@ app.use("/api/owner", ownController)
 app.use("/api/property", propController)
 app.use("/api/job", jobController)
 
-app.set("port", process.env.PORT || 4040);
+// app.set("port", process.env.PORT || 8000);
+// app.listen(app.get("port"), () => {
+//     console.log(`PORT: ${app.get("port")} works`)
+// })
+
+
+app.set("port", process.env.PORT || 8000);
+
 app.listen(app.get("port"), () => {
-    console.log(`PORT: ${app.get("port")} works`)
-})
+    console.log(`✅ PORT: ${app.get("port")}`);
+});
