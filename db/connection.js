f222d49c38667c9f6c009599c9a01f875605720c
DCartist <pbannerman13@gmail.com>
2019-06-09 23:02:33 -0400

    fixing

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 mongoose.Promise = Promise
-let mongoURI = ""
-
+let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
-    mongoURI = process.env.DB_URL
+    mongoURI = process.env.DB_URL;
 } else {
-    mongoURI = "mongodb://localhost/nycjobapp"
+    mongoURI = "mongodb://localhost/nycjobapp";
 }
+
 mongoose
     .connect(mongoURI, { useNewUrlParser: true })
     .then(instance => console.log(`Connected to db: ${instance.connections[0].name}`)) //instance is what the database that your are connected to
