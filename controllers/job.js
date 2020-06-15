198d37a873ac3883fe6a6138d7aec3b8ca7292b2
DCartist <pbannerman13@gmail.com>
2020-06-15 12:43:49 -0400

    updating job controller

--- controllers/job.js
+++ controllers/job.js
@@ -5,26 +5,51 @@ const Contractor = require('../models/Contractor.js')
 const Jobs = require('../models/Jobs.js')
 const Owner = require('../models/Owner.js')
 
-router.get("/", (req,res) => {
-    res.send("Welcome")
+router.get("/", (req, res) => {
+  Jobs.find()
+      .populate("property")
+      .populate("owner")
+      .populate("contractor")
+      .then(jobs => {
+          res.json(jobs)
+      })
 })
 
-router.get("/:jobId", (req, res) => {
 
-    // let finalResults = {}
-    Contractor.find({  jobId: req.params.jobId }).then( results => {
-       let finalResults = Object.assign({}, results)
-    //    res.json(finalResults)
-    return finalResults
-    })
-    Property.find({  jobId: req.params.jobId }).then(  results => {
-        let finalResults1 = Object.assign(results)
-        let mainResults = typeof finalResults1
-       res.send(mainResults)
-    }).catch(err => res.send(err))
-    // Owner.find({  jobId: req.params.jobId }).then(  results => {
-    //     finalResults = {...results}
-    // }).then(res.json(finalResults))
+router.get("/id/:jobId", (req, res) => {
+  let thejob = req.params.jobId
+  Jobs.find({ jobId: thejob })
+      .populate("contractor")
+      .populate("property")
+      .populate("owner")
+      .then(showinfo => res.json(showinfo))
+})
+
+// router.get("/", (req,res) => {
+//     res.send("Welcome")
+// })
+// let resultObj = {}
+// let  insertObj = (objName) => {
+//     Object.keys(objName).forEach((item, index) => resultObj[item]=objName[item])
+//     return resultObj
+//  }
+  
+
+
+// router.get("/:jobId", (req, res) => {
+
+//     Contractor.find({  jobId: req.params.jobId }).then( results => {
+//        insertObj(results)
+//     })
+//     Owner.find({  jobId: req.params.jobId }).then( results => {
+//       return insertObj(results)
+//     })
+//     Property.find({  jobId: req.params.jobId }).then( results => {
+//       insertObj(results)
+//     }).then(
+//         res.json(resultObj)
+//     ).catch(err => res.send(err))
+    
 
 })
 // router.get("/", (req, res) => {
