a50767858a3aadba1d910078e6cef373e9661b2b
dcartist <pbannerman13@gmail.com>
2024-08-29 11:52:49 -0400

    adding to the connection

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,17 @@ let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
     mongoURI = process.env.DB_URL;
 } else {
-    mongoURI = "mongodb://localhost/nycjobapp";
+    mongoURI = "mongodb://localhost/nycjobappv2";
     // mongoURI = process.env.DB_URL;
     // mongoURI = "mongodb://localhost/nycjobapp";
 }
 mongoose
-    .connect(mongoURI, { useNewUrlParser: true })
+    .connect(mongoURI, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 50000,
+        socketTimeoutMS: 65000 
+     })
     .then(instance => console.log(`Connected to db: ${instance.connections[0].name}`)) //instance is what the database that your are connected to
     .catch(error => console.log('Connection failed!', error))
 
