fc52b6fef63aa38d8b50532581cf03fdc2b2fd6c
dcartist <pbannerman13@gmail.com>
2025-07-15 08:38:32 -0400

    creating seed file

--- controllers/v2/index.js
+++ controllers/v2/index.js
@@ -4,10 +4,22 @@ const jobsController = require("./jobs.js");
 const applicationsController = require("./applications.js");
 const propertyController = require("./property.js");
 const contractorsController = require("./contractors.js");
+const seedData = require("../../db/v2/seed.js");
+
+router.post("/seed", async (req, res) => {
+  try {
+    await seedData();
+    res.json({ message: "Seeding completed successfully." });
+  } catch (error) {
+    res.status(500).json({ error: "Seeding failed.", details: error.message });
+  }
+});
 
 router.get("/", (req, res) => {
    res.send("Welcome to the API v2");
 })
+
+
 
 router.use("/jobs", jobsController);
 router.use("/applications", applicationsController);
