da22a010a55d228054455edfcdaa0af081d99d59
dcartist <pbannerman13@gmail.com>
2026-03-16 16:22:59 -0400

    updating app js

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const swaggerHtml = `<!DOCTYPE html>
     <style>.swagger-ui .topbar { display: none }</style>
   </head>
   <body>
+  
     <div id="swagger-ui"></div>
     <script src="https://unpkg.com/swagger-ui-dist@5.31.0/swagger-ui-bundle.js"></script>
     <script src="https://unpkg.com/swagger-ui-dist@5.31.0/swagger-ui-standalone-preset.js"></script>
